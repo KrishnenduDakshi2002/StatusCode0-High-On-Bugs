6efdbc075fab6d1a6c405f6d7ea642c61b3e0f2c
Debayan <pradhandebayan@gmail.com>
2023-08-12 12:04:01 +0530

    added footer

---
+++ apps/crypto-bank-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+// src/components/Footer.js
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <ul className="footer__links">
+          <li className="footer__link-item">
+            <a href="/contact">Contact Us</a>
+          </li>
+          <li className="footer__link-item">
+            <a href="https://twitter.com">Twitter</a>
+          </li>
+          <li className="footer__link-item">
+            <a href="https://youtube.com">YouTube</a>
+          </li>
+          {/* Add more social media links as needed */}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
