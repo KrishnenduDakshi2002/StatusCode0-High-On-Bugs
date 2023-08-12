9a9fc675f4b6641ca85e671ed7e94a9cb2fc488c
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-08-12 11:29:25 +0530

    initial commit - nx: reactjs and nestjs

---
+++ apps/crypto-bank-backend/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'crypto-bank-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/crypto-bank-backend',
+};
