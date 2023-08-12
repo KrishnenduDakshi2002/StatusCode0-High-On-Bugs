a83317818a4e43c36fe177ae81c9918f5eaa2e3d
Debayan <pradhandebayan@gmail.com>
2023-08-12 20:08:39 +0530

    updated

--- apps/crypto-bank-frontend/src/pages/Home/Home.tsx
+++ apps/crypto-bank-frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './Home.css';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full">
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
